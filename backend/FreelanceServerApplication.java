ad537ed6f95070155ae61201ce11c7b9c43ec5c3
Grigor <120247466+LevVasilev@users.noreply.github.com>
2025-05-19 23:55:24 +0300

    Add files via upload

---
+++ backend/FreelanceServerApplication.java
@@ -0,0 +1,14 @@
+package com.platform.freelance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FreelanceServerApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(FreelanceServerApplication.class, args);
+    }
+
+}
